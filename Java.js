512571738ac6fd09e565766e5f5178f6cd776bf1
Alan Monkman <almonkman27@gmail.com>
2022-01-06 13:38:08 -0800

    noemail

--- Java.js
+++ Java.js
@@ -1,7 +1,7 @@
 let name = 'banana';
 let age = prompt('what is your age?');
+
 let lovesAM = confirm('do you love AM?');
-let email = prompt('What is your email?');
 
 if (age >=26){document.write('You\re Young!');
 }
