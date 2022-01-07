db59027e9570edcf92748147b9802535f94b9e64
Alan Monkman <almonkman27@gmail.com>
2022-01-07 12:04:47 -0800

    maplebars!

--- Java.js
+++ Java.js
@@ -1,13 +1,13 @@
 let age;
 
 
+
 function something(){
 
     alert("Wazuuuppppppp");
 
     age = prompt('What is your age?');
-
-    let lovesAM = confirm('Do you love AM');
+ 
 }
 
 something()
@@ -19,3 +19,31 @@ if (age >26) {
 else {
     document.write('You\'re Young!');
 }
+
+function loop(){
+
+    let lovesAM = confirm('Do you love AM'); 
+
+    while(!lovesAM){
+        lovesAM=confirm('Are you sure you don\'t?');
+
+    }
+
+
+}
+
+loop();
+
+function Donut(){
+    let Maple = prompt('How many maple bars can you eat?');
+
+    for (let i=0; i <Maple;i++) {
+        document.write('<img src="MapleBar.jpg" alt="maplebars" class="donuts"></img>');
+    }
+
+
+
+
+}
+
+Donut();
